8122d94bce4d5c23c0fbfa0cad6f08b22f9b778b
Alberto Castañeiras <alberto@casta.me>
2023-12-14 15:45:31 +0100

    Add copy functions

--- dir/dir.go
+++ dir/dir.go
@@ -1,4 +1,58 @@
 package dir
 
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+
+	_file "github.com/vekio/fs/file"
+)
+
 // DefaultPerms are defaults for new directory creation.
 var DefaultPerms = 0755
+
+// Create a new directory with the DefaultPerms creating any new
+// directories as well (see os.MkdirAll).
+func Create(path string) error {
+	return os.MkdirAll(path, fs.FileMode(DefaultPerms))
+}
+
+// Copy recursively the contents of the source directory
+// to the destination directory.
+func Copy(src, dst string) error {
+	// Get information about the source directory
+	info, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	// Create the destination directory if it does not exist
+	if err := os.MkdirAll(dst, info.Mode()); err != nil {
+		return err
+	}
+
+	// List files in the source directory
+	files, err := filepath.Glob(filepath.Join(src, "*"))
+	if err != nil {
+		return err
+	}
+
+	// Copy each file/directory to the destination directory
+	for _, file := range files {
+		newPath := filepath.Join(dst, filepath.Base(file))
+
+		// If it's a directory, recursively call Copy
+		if info, err := os.Stat(file); err == nil && info.IsDir() {
+			if err := Copy(file, newPath); err != nil {
+				return err
+			}
+		} else {
+			// If it's a file, copy it
+			if err := _file.Copy(file, newPath); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
--- file/file.go
+++ file/file.go
@@ -1,1 +1,90 @@
 package file
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// DefaultPerms for new file creation.
+var DefaultPerms = 0600
+
+// Copy copies a file from src to dst. If src and dst files exist, and are
+// the same, then return success. Otherise, attempt to create a hard link
+// between the two files. If that fail, copy the file contents from src to dst.
+func Copy(src, dst string) (err error) {
+	// Get information about the source file
+	sfi, err := os.Stat(src)
+	if err != nil {
+		return
+	}
+
+	// Check if the source file is a regular file
+	if !sfi.Mode().IsRegular() {
+		// Cannot copy non-regular files
+		// (e.g., directories, symbolic links, devices, etc.)
+		return fmt.Errorf("CopyFile: non-regular source file %s (%q)",
+			sfi.Name(), sfi.Mode().String())
+	}
+
+	// Get information about the destination file, if it exists
+	dfi, err := os.Stat(dst)
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+
+	// Check if the destination file is a regular file
+	if err == nil && !dfi.Mode().IsRegular() {
+		return fmt.Errorf("CopyFile: non-regular destination file %s (%q)",
+			dfi.Name(), dfi.Mode().String())
+	}
+
+	// Check if the source and destination files are the same
+	if err == nil && os.SameFile(sfi, dfi) {
+		return
+	}
+
+	// Try creating a hard link between the source and destination files
+	if err = os.Link(src, dst); err == nil {
+		return
+	}
+
+	// If creating a hard link fails,
+	// copy the contents of the source file to the destination file
+	err = copyFileContents(src, dst)
+	return
+}
+
+// copyFileContents copies the contents of the file named src to the file named
+// by dst. The file will be created if it does not already exist. If the
+// destination file exists, all it's contents will be replaced by the contents
+// of the source file.
+func copyFileContents(src, dst string) (err error) {
+	// Open the source file for reading
+	in, err := os.Open(src)
+	if err != nil {
+		return
+	}
+	defer in.Close()
+
+	// Create the destination file for writing
+	out, err := os.Create(dst)
+	if err != nil {
+		return
+	}
+	defer func() {
+		cerr := out.Close()
+		if err == nil {
+			err = cerr
+		}
+	}()
+
+	// Copy the contents from the source file to the destination file
+	if _, err = io.Copy(out, in); err != nil {
+		return
+	}
+
+	// Ensure all contents are written to disk
+	err = out.Sync()
+	return
+}
