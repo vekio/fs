4a3bda4789bbace9333ea93e6c48a8803bfdc3ab
Alberto Castañeiras <alberto@casta.me>
2024-08-27 23:34:37 +0200

    Add direnv name

--- flake.nix
+++ flake.nix
@@ -7,21 +7,24 @@
     let
       goVersion = 22; # Change this to update the whole stack
 
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.default ];
-        };
-      });
-    in
-    {
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems (system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ self.overlays.default ];
+            };
+          });
+    in {
       overlays.default = final: prev: {
         go = final."go_1_${toString goVersion}";
       };
 
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
+          name = "fs";
           packages = with pkgs; [
             # go (version is specified by overlay)
             go
